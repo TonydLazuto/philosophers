e33c714ea0e92f233b4fadd321539c4bb08a3400
Anthony DEROSE <aderose@42.student.fr>
2021-10-14 19:24:25 +0200

    first

---
+++ Makefile
@@ -0,0 +1,34 @@
+.PHONY		= all clean fclean re bonus
+
+NAME		= philo
+
+CC			= gcc
+
+CFLAGS		= -Wall -Wextra -Werror
+
+RM			= /bin/rm -f
+
+INC			= philo.h
+
+OBJ_DIR		= obj
+
+SRCS		= philo.c
+
+OBJS		= $(addprefix $(OBJ_DIR)/,$(SRCS:.c=.o))
+
+all: 			$(NAME)
+
+$(NAME): 		$(OBJS)
+				$(CC) -o $(NAME) $(OBJS)
+
+$(OBJ_DIR)/%.o:	%.c
+				@mkdir -p $(OBJ_DIR)
+				$(CC) $(CFLAGS) -c $< -o $@
+
+clean:			
+				rm -rf $(OBJ_DIR)
+
+fclean:			clean
+				$(RM) $(NAME)
+
+re:				fclean all
---
+++ philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aderose <aderose@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/14 18:47:08 by aderose           #+#    #+#             */
+/*   Updated: 2021/10/14 18:47:42 by aderose          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+#include <string.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+# endif
