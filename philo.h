81f02eb47e145ab29062588d6cc710f598d81912
TonydLazuto <aderose73@gmail.com>
2022-12-21 12:16:24 +0100

    nb_lunch + right_fork

--- philo.h
+++ philo.h
@@ -50,13 +50,12 @@ typedef struct s_philo
 {
 	int				num;
 	t_info			*info;
-	pthread_mutex_t	*mut;
-	pthread_mutex_t	*right_fork;
+	pthread_mutex_t	mut;
+	pthread_mutex_t	nb_lunch;
+	pthread_mutex_t	right_fork;
 	pthread_mutex_t	*left_fork;
 	long			nb_meals_eaten;
 	long			last_meal;
-	struct s_philo	*left;
-	struct s_philo	*right;
 }				t_philo;
 
 long		ft_atoi(const char *str);
@@ -70,17 +69,12 @@ char		*ft_strcat(char *dst, const char *src);
 void		print_msg(t_philo *phil, char *state);
 
 int			check_args(int ac, char *av[]);
-int			check_info(t_info *info);
-void		push_back(t_philo **phil, int num, t_info *info);
-t_philo		*last_philo(t_philo *elet);
-int			clear_philos(t_philo **phil);
-
+int			check_limits_values(t_info *info);
 t_philo		*init(char *av[], t_philo *phil, t_info *info);
-void		free_mutex(pthread_mutex_t *mut);
 
 long		get_time(void);
-void		ft_usleep(long time);
-long		get_current_time(long start);
+void		ft_usleep(long long time);
+long long	get_current_time(long long start);
 
 void		*death_routine(void *data);
 void		*routine(void *data);
